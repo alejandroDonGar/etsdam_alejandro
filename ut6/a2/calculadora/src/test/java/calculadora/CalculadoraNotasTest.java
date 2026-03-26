e9453d3865d485f8b68d50b2fa9dda49169c943c
Alejandro Donate Garcia <alexdoga.work@gmail.com>
2026-03-26 18:45:30 +0000

    Se arregló la calculadora de notas

--- ut6/a2/calculadora/src/test/java/calculadora/CalculadoraNotasTest.java
+++ ut6/a2/calculadora/src/test/java/calculadora/CalculadoraNotasTest.java
@@ -1,26 +1,28 @@
 package calculadora.src.test.java.calculadora;
-
+/**
+ * @author AlejandroDonGar
+ * @version 1.0.0
+ * Clase CalculadoraNotasTest
+ */
 import calculadora.src.main.java.calculadora.CalculadoraNotas;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-
+/**
+ * Clase CalculadoraNotasTest donde probaremos distintos test para la seguridad del codigo
+ */
 public class CalculadoraNotasTest {
-
     @Test
     void testMediaSimple() {
     assertEquals(7.0, CalculadoraNotas.calcularMedia(new int[] { 6, 7, 8 }), 0.0001);
     }
-
     @Test
     void testMediaDecimal() {
     assertEquals(8.5, CalculadoraNotas.calcularMedia(new int[] { 10, 9, 8, 7 }), 0.0001);
     }
-
     @Test
     void testMediaListaVacia() {
     assertThrows(IllegalArgumentException.class, () -> CalculadoraNotas.calcularMedia(new int[] {}));
     }
-
     @Test
     void testMediaNull() {
     assertThrows(IllegalArgumentException.class, () -> CalculadoraNotas.calcularMedia(null));
