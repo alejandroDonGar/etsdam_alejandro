76dd009af0cd8dd7bc848ad31a5d68f741338842
Alejandro Donate Garcia <AlexDoGa.work@gmail.com>
2026-04-07 01:11:02 +0100

    Actividad ut6.a3 Terminada

---
+++ ut6/a3/calculadora/src/test/java/calculadora/CalculadoraNotasTest.java
@@ -0,0 +1,46 @@
+package calculadora;
+/**
+ * @author AlejandroDonGar
+ * @version 1.0.0
+ * Clase CalculadoraNotas que usamos para calcular notas 
+ */
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+/**
+ * Clase CalculadoraNotasTest donde probaremos distintos test para la seguridad del codigo
+ */
+public class CalculadoraNotasTest {
+    /**
+     * Este test comprueba que el calculo de la nota media es correcto
+     */
+    @Test
+    void testMediaSimple() {
+        assertEquals(7.0, CalculadoraNotas.calcularMedia(new int[]{6, 7, 8}));
+    }
+    /**
+     * Este test comprueba que el calculo de la nota media es correcto con decimales
+     */
+    @Test
+    void testMediaDecimal() {
+        assertEquals(8.5, CalculadoraNotas.calcularMedia(new int[]{10, 9, 8, 7}));
+    }
+    /**
+     * Este test salta cuando se detecta que el calculo de la nota media es vacio
+     */
+    @Test
+    void testListaVacia() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            CalculadoraNotas.calcularMedia(new int[]{});
+        });
+    }
+    /**
+     * Este test salta cuando se detecta que el calculo de la nota media es nulo
+     */
+    @Test
+    void testNotaFueraDeRango() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            CalculadoraNotas.calcularMedia(new int[]{5, 12, 8});
+        });
+    }
+}
